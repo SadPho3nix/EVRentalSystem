5093e2c287110356ed2a84e812ca30ef1a46d5cf
Nguyen Ngoc Bao Cuong <Firephoenix0304@gmail.com>
2025-09-22 07:20:21 +0700

    Add files via upload

---
+++ EVRentalSystem/src/main/java/com/evrentalsystem/EVRentalSystem/EvRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.evrentalsystem.EVRentalSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvRentalSystemApplication.class, args);
+	}
+
+}
---
+++ EVRentalSystem/src/test/java/com/evrentalsystem/EVRentalSystem/EvRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evrentalsystem.EVRentalSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
