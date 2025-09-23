4d42fdaa2fc85cfca18b70fd5b92bed21e99de1a
tn-1412 <trungnguyen14122005@gmail.com>
2025-09-23 10:24:01 +0700

    Hoàn thành 3 entities: Station/Vehicle/VehicleDetail

---
+++ EVRentalSystem/src/main/java/com/evrentalsystem/evRentalSystem/entity/VehicleDetail.java
@@ -0,0 +1,34 @@
+package com.evrentalsystem.evRentalSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Vehicle_Detail")
+public class VehicleDetail {
+
+    @Id
+    @Column(name = "license_plate", length = 50)
+    private String licensePlate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id", nullable = false)
+    private Vehicle vehicle;
+
+    @Column(length = 50)
+    private String color;
+
+    private Integer seats;
+
+    @Column(name = "battery_capacity", length = 50)
+    private String batteryCapacity;
+
+    private Integer odo;
+
+    @Column(nullable = false, length = 20)
+    private String status = "AVAILABLE";
+}
